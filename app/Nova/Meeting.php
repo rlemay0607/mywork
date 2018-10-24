2fc344f8a29af4ed6046fd7521a986ec49c22a69
ryanlemay0607 <lemay.ryan@gmail.com>
2018-10-24 18:07:25 -0400

    Add plugin

--- app/Nova/Meeting.php
+++ app/Nova/Meeting.php
@@ -55,6 +55,7 @@ class Meeting extends Resource
             Trix::make('Details')
                 ->sortable()
                 ->rules('required'),
+            HasMany::make('Notes'),
 
 
         ];
