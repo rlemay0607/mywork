82eaf90d7b4e18317844ccbf731cf5c18854e65d
ryanlemay0607 <lemay.ryan@gmail.com>
2018-11-04 11:01:33 -0500

    Add plugin

---
+++ app/Nova/Metrics/NotesPerDay.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Nova\Metrics;
+
+use App\Note;
+use Illuminate\Http\Request;
+use Laravel\Nova\Metrics\Trend;
+
+class NotesPerDay extends Trend
+{
+    /**
+     * Calculate the value of the metric.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return mixed
+     */
+    public function calculate(Request $request)
+    {
+        return $this->countByDays($request, Note::class)
+            ->showLatestValue();
+    }
+
+    /**
+     * Get the ranges available for the metric.
+     *
+     * @return array
+     */
+    public function ranges()
+    {
+        return [
+            5 => '5 Days',
+            10 => '10 Days',
+            15 => '15 Days',
+            20 => '20 Days',
+            25 => '25 Days',
+            30 => '30 Days',
+            60 => '60 Days',
+            90 => '90 Days',
+        ];
+    }
+
+    /**
+     * Determine for how many minutes the metric should be cached.
+     *
+     * @return  \DateTimeInterface|\DateInterval|float|int
+     */
+    public function cacheFor()
+    {
+        // return now()->addMinutes(5);
+    }
+
+    /**
+     * Get the URI key for the metric.
+     *
+     * @return string
+     */
+    public function uriKey()
+    {
+        return 'notes-per-day';
+    }
+}
